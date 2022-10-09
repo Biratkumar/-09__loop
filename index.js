13bca24f0d74c8a797d87208d8d1face720147dd
birat kumar <97181904+Biratkumar@users.noreply.github.com>
2022-10-09 04:56:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//programe to add n Natural numbers
+/*
+let sum = 0
+let n = prompt("Enter the value of n")
+n= Number.parseInt(n)
+for ( let i=0; i<n; i++){
+  sum += (i + 1)
+}
+  console .log("sum of first" + n +"natural numbers is" + sum)
+*/
+//while loop
+/*
++let n= prompt("Enter the value of n")
+n = Number.parseInt(n)
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+   
+let n= prompt("Enter the value of n")
+n = Number.parseInt(n)
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+   
